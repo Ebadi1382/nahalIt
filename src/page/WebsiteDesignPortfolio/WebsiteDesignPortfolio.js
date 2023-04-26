7de96a441783fcd8b8d2764ab8f2b4be4f42d714
AlirezaEbadi <ebadia256@gmail.com>
2023-04-26 17:32:05 +0330

    responsive portfolio page

--- src/page/WebsiteDesignPortfolio/WebsiteDesignPortfolio.js
+++ src/page/WebsiteDesignPortfolio/WebsiteDesignPortfolio.js
@@ -16,20 +16,24 @@ const WebsiteDesignPortfolio = () => {
           <ResponseHeader />
         </div>
       </header>
-      <div className="flex items-start justify-between py-10 container mx-auto px-48">
-        <div>
-          <p className="font-[shabnamBold] text-xl tracking-tighter text-[#4c4c4c] mb-2">نمونه کارها</p>
-          <Link className="text-[#00750A] font-[shabnamBold] tracking-tighter text-lg">سایت کفش توپ</Link>
-          <p className="text-lg tracking-tighter">این سایت با موضوع خرید کفش و کتونی با نام کفش توپ می باشد .</p>
+      <div className="flex flex-col flex-wrap max-lg:items-center items-center justify-center py-10 container mx-auto px-48 max-lg:px-20 max-md:px-12 max-sm:px-5">
+        <h2 className="font-[shabnamBold] flex items-center justify-center text-xl tracking-tighter text-[#4c4c4c] mb-2">
+          نمونه کارها
+        </h2>
+        <div className="flex flex-wrap items-center justify-between max-lg:justify-center max-lg:mt-8">
+          <div>
+            <Link className="text-[#00750A] font-[shabnamBold] tracking-tighter text-lg">سایت کفش توپ</Link>
+            <p className="text-lg tracking-tighter">این سایت با موضوع خرید کفش و کتونی با نام کفش توپ می باشد .</p>
+          </div>
+          <Link className="flex items-center justify-center">
+            <img
+              src="https://nahalit.com/wp-content/uploads/2022/10/photo_2022-10-07_03-12-07.jpg"
+              className="rounded-3xl border-[3.1px] border-solid border-[#4c4c4c] m-8 hover:opacity-80 transition-all duration-300"
+              width={320}
+              alt="image_portfolio"
+            />
+          </Link>
         </div>
-        <Link>
-          <img
-            src="https://nahalit.com/wp-content/uploads/2022/10/photo_2022-10-07_03-12-07.jpg"
-            className="rounded-3xl border-[3.1px] border-solid border-[#4c4c4c] m-8 hover:opacity-80 transition-all duration-300"
-            width={320}
-            alt="image_portfolio"
-          />
-        </Link>
       </div>
       <div>
         <FixedIcon />
